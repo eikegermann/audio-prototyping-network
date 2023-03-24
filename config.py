6e827589ff6ed7703303c1cffdf60105dd0444b3
Eike Germann <eike.germann@gmail.com>
2023-03-24 04:30:14 +0000

    update_variables

---
+++ config.py
@@ -0,0 +1,70 @@
+# Parameters
+num_runs = 1
+
+sr = 44100
+duration = 0.8
+n_samples = int(sr * duration)
+
+# Parameters for spectrograms
+n_fft = int(4096 * 3/4)
+hop_length = n_fft // 4
+n_frames = librosa.time_to_frames(duration,
+                                  sr=sr,
+                                  hop_length=hop_length,
+                                  n_fft=n_fft)
+num_freqs = int(n_fft / 2 + 1)
+
+# Parameters for custom spectrograms
+fmin = 20
+fmax = int(sr / 2)
+fraction = 1/40
+num_filters = number_of_filterbands(fmin, fmax, fraction)
+filterbank = create_custom_filterbank(sr, n_fft, fraction, fmin, fmax)
+
+
+# Special parameters for mel-spectrograms
+n_mels = 128
+
+# # Special parameters for wavelet spectrograms (scalograms)
+# lower_bound = 20
+# upper_bound = 20000
+# num_voices = 12
+# scales = create_scales(lower_bound, upper_bound, num_voices, sr)
+# fixed_length = 128
+
+#### Set up training parameters
+
+# Training data
+data_folder = 'data/firearm_samples/train/'
+
+preprocessing_function = generate_spectrogram
+
+num_bands = num_freqs
+fixed_length = n_frames
+fraction_for_train = 0.7
+num_episodes = 2
+num_batch_labels_train = int(num_data_folders * fraction_for_train)
+display_interval = 25
+n_features = 50
+learning_rate = 1e-3
+weight_decay = 0.0002
+support_ratio_train = 0.6
+
+#### Set up evaluation parameters
+
+# Test data
+eval_data_dir = 'data/firearm_samples/test/'
+
+fraction_for_eval = 1
+num_eval_episodes = 100
+num_batch_labels_eval = int(num_data_folders * fraction_for_eval)
+support_ratio_eval = 0.6
+
+min_classes_per_batch = 2
+batch_size_support = 12
+batch_size_query = 12
+min_class_appearances = 10  # Set a minimum number of appearances for each class
+num_batches = 30
+
+# Checkpoint directory
+checkpoint_dir = "checkpoints"
--- train.py
+++ train.py
@@ -1,120 +1,63 @@
 import os
 import copy
-import math
 import random
-import librosa
 import librosa.display
-import itertools
-import pywt
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-import IPython.display as ipd
-from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
-from IPython.display import Audio, display
 from torchsummary import summary
+
+from source.dataset import CustomDataset
+from source.architecture import AudioClassifier
+from source.data_utils import generate_spectrogram
+import config
+
 
 # Run multiple training runs to find best checkpoint
 
-# Parameters
-num_runs = 1
+# Get number of classes
+num_data_folders = len([subfolder for subfolder in os.listdir(config.data_folder)
+                        if os.path.isdir(os.path.join(config.data_folder, subfolder))])
 
-sr = 44100
-duration = 0.8
-n_samples = int(sr * duration)
+train_dataset = CustomDataset(config.data_folder,
+                              config.preprocessing_function,
+                              config.sr,
+                              config.duration,
+                              config.n_fft,
+                              config.fixed_length)
 
-# Parameters for spectrograms
-n_fft = int(4096 * 3/4)
-hop_length = n_fft // 4
-n_frames = librosa.time_to_frames(duration,
-                                  sr=sr,
-                                  hop_length=hop_length,
-                                  n_fft=n_fft)
-num_freqs = int(n_fft / 2 + 1)
-
-# Parameters for custom spectrograms
-fmin = 20
-fmax = int(sr / 2)
-fraction = 1/40
-num_filters = number_of_filterbands(fmin, fmax, fraction)
-filterbank = create_custom_filterbank(sr, n_fft, fraction, fmin, fmax)
-
-
-# Special parameters for mel-spectrograms
-n_mels = 128
-
-# # Special parameters for wavelet spectrograms (scalograms)
-# lower_bound = 20
-# upper_bound = 20000
-# num_voices = 12
-# scales = create_scales(lower_bound, upper_bound, num_voices, sr)
-# fixed_length = 128
-
-#### Set up training parameters
-
-# Training data
-data_folder = 'data/firearm_samples/train/'
-num_data_folders = len([subfolder for subfolder in os.listdir(data_folder) if os.path.isdir(os.path.join(data_folder, subfolder))])
-preprocessing_function = generate_spectrogram
-
-num_bands = num_freqs
-fixed_length = n_frames
-fraction_for_train = 0.7
-num_episodes = 2
-num_batch_labels_train = int(num_data_folders * fraction_for_train)
-display_interval = 25
-n_features = 50
-learning_rate = 1e-3
-weight_decay = 0.0002
-support_ratio_train = 0.6
-
-#### Set up evaluation parameters
-
-# Test data
-eval_data_dir = 'data/firearm_samples/test/'
-
-fraction_for_eval = 1
-num_eval_episodes = 100
-num_batch_labels_eval = int(num_data_folders * fraction_for_eval)
-support_ratio_eval = 0.6
-
-min_classes_per_batch = 2
-batch_size_support = 12
-batch_size_query = 12
-min_class_appearances = 10  # Set a minimum number of appearances for each class
-num_batches = 30
-
-train_dataset = CustomDataset(data_folder, preprocessing_function, sr, duration, n_fft, fixed_length)
-
-test_dataset = CustomDataset(eval_data_dir, preprocessing_function, sr, duration, n_fft, fixed_length)
+test_dataset = CustomDataset(config.eval_data_dir,
+                              config.preprocessing_function,
+                              config.sr,
+                              config.duration,
+                              config.n_fft,
+                              config.fixed_length)
+                             
 
 # Set variable to compare evaluation accuracies
 best_accuracy = 0.0
 best_f1_score = 0.0
 
 # Create a directory to save the best checkpoint
-checkpoint_dir = "checkpoints"
-os.makedirs(checkpoint_dir, exist_ok=True)
+os.makedirs(config.checkpoint_dir, exist_ok=True)
 
-for model_run in range(num_runs):
+for model_run in range(config.num_runs):
     print(f"Model run: {model_run}")
     #### Training Phase
     # Instantiate the embedding model
-    embedding_model = AudioClassifier(n_bands=num_bands,
-                                      n_frames=fixed_length,
-                                      n_features=n_features)
+    embedding_model = AudioClassifier(n_bands=config.num_bands,
+                                      n_frames=config.fixed_length,
+                                      n_features=config.n_features)
 
     # Set up the loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(embedding_model.parameters(),
-                          lr=learning_rate,
-                          weight_decay=weight_decay)
+                          lr=config.learning_rate,
+                          weight_decay=config.weight_decay)
     
     # # Create the CosineAnnealingLR scheduler
     # T_max = 45  # You can choose an appropriate value for T_max
@@ -133,15 +76,18 @@ for model_run in range(num_runs):
     all_predictions = []
 
     class_counter = {class_label: 0 for class_label in range(num_data_folders)}
-    all_classes_represented = all(count >= min_class_appearances for count in class_counter.values())
+    all_classes_represented = all(count >= config.min_class_appearances for count in class_counter.values())
     step_num = 0
 
     # Training loop
-    for episode in range(num_episodes):
+    for episode in range(config.num_episodes):
 
-        train_support_loader, train_query_loader = train_dataset.get_support_query_dataloaders(min_classes_per_batch, batch_size_support, batch_size_query, num_batches)
+        train_support_loader, train_query_loader = train_dataset.get_support_query_dataloaders(config.min_classes_per_batch,
+                                                                                               config.batch_size_support,
+                                                                                               config.batch_size_query,
+                                                                                               config.num_batches)
 
-        while step_num < num_batches:
+        while step_num < config.num_batches:
             # Load one batch from support and query DataLoaders
             support_set, support_labels = next(iter(train_support_loader))
             query_set, query_labels = next(iter(train_query_loader))
@@ -223,10 +169,10 @@ for model_run in range(num_runs):
     all_eval_predictions = []
 
     # Evaluation loop
-    for episode in range(num_eval_episodes):
+    for episode in range(config.num_eval_episodes):
         # Randomly select a subset of classes
         available_classes = torch.unique(eval_labels_tensor)
-        batch_labels = available_classes[torch.randperm(len(available_classes))[:num_batch_labels_eval]]
+        batch_labels = available_classes[torch.randperm(len(available_classes))[:config.num_batch_labels_eval]]
         batch_labels = batch_labels.to(device)
 
         # Initialize support and query sets
@@ -239,7 +185,7 @@ for model_run in range(num_runs):
         for class_label in batch_labels:
             class_indices = [i for i, label in enumerate(eval_labels) if label == class_label]
             random.shuffle(class_indices)
-            n_support = int(support_ratio_eval * len(class_indices))
+            n_support = int(config.support_ratio_eval * len(class_indices))
             support_indices = class_indices[:n_support]
             query_indices = class_indices[n_support:]
 
@@ -289,8 +235,8 @@ for model_run in range(num_runs):
         all_eval_predictions.extend(predictions.cpu().numpy())
 
     # Calculate evaluation metrics
-    eval_avg_loss = eval_total_loss / num_eval_episodes
-    eval_avg_accuracy = eval_total_accuracy / num_eval_episodes
+    eval_avg_loss = eval_total_loss / config.num_eval_episodes
+    eval_avg_accuracy = eval_total_accuracy / config.num_eval_episodes
     f1 = f1_score(all_eval_true_labels, all_eval_predictions, average='weighted')
 
     print(f"Evaluation Average Loss: {eval_avg_loss:.4f}, Average Accuracy: {eval_avg_accuracy:.4f}, F1 Score: {f1:.4f} \n")
@@ -300,6 +246,6 @@ for model_run in range(num_runs):
         best_f1_score = f1
         best_model_weights = copy.deepcopy(embedding_model.state_dict())
         print("Saving new best checkpoint... \n")
-        torch.save(best_model_weights, os.path.join(checkpoint_dir, 'best_checkpoint.pth'))
+        torch.save(best_model_weights, os.path.join(config.checkpoint_dir, 'best_checkpoint.pth'))
 
 print(f"Evaluation complete. Saved checkpoint has F1 score of {best_f1_score:.2f}")
